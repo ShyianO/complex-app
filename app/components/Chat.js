7c868dd59b40d9ac4333c60aaa80f25837eb99ee
ShyianO <ol.shyian@gmail.com>
2020-08-18 13:51:46 +0300

    Added netlify

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -25,7 +25,10 @@ function Chat() {
   }, [appState.isChatOpen]);
 
   useEffect(() => {
-    socket.current = io("http://localhost:8080");
+    socket.current = io(
+      process.env.BACKENDURL ||
+        "https://backendformycomplexreactapp.herokuapp.com"
+    );
 
     socket.current.on("chatFromServer", (message) => {
       setState((draft) => {
